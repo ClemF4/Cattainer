26f1d4e51da406be0cc635357d88171a46b7d532
ClemF4 <clementfau4@gmail.com>
2026-02-16 21:16:29 +0100

    added flask cors to handle security conflicts between browser and flask server. Added it to requirements.txt

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_cors import CORS  # installed flask-cors to handle security conflicts between browsers and the server
 
 app = Flask(__name__)
+CORS(app)  # This line adds something to the header telling my browser that this site is safe
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.after_request
+def add_header(response):
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
