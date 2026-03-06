0214e0204add94f2eedbc692336c2ceb3bc0c9ee
ClemF4 <clementfau4@gmail.com>
2026-03-06 12:47:29 +0000

    added loggers default config & started initaliseCamera function which starts the camera & checks whether it is working or not

--- cattainer.py
+++ cattainer.py
@@ -1,13 +1,31 @@
 import time
 import cv2
 import os
+import logging
 from picamera2 import Picamera2
 from ultralytics import YOLO
 
 OUTPUT_DIR = 'output_frames'
 
+logging.basicConfig(
+    filename = 'cattainer.log',
+    level = logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def initialiseCamera():
+    try:
+        #If the camera isnt connected this line will cause an error & go straight to except
+        picam2 = Picamera2()
+        return picam2
+    except:
+        return
+
 def catDetect():
-    
+
 
 if __name__ == "__main__":
-    catDetect()
+    initialiseCamera()
+    
+
+    
