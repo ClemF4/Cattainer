22c7ea58f4537e34f1946f0b2008755d76058f25
ClemF4 <clementfau4@gmail.com>
2026-03-08 15:25:02 +0000

    removed flask-cors from requirements, and improved my comments on app.py

--- app.py
+++ app.py
@@ -17,17 +17,20 @@ def saved_zones():
         json.dump(zones_data, file, indent=4)
     return jsonify({"status": "success"}), 200
 
+#Route the flag file here, it creates a new empty file on my system called trigger_snapshot.flag
 @app.route('/triggerSnapshot', methods=['POST'])
 def trigger_snapshot():
     with open('trigger_snapshot.flag', 'w') as file:
         pass
     return jsonify({"status": "success"}), 200
 
+#Allow any website to use my website in an iframe on their website (necessary for HA)
+#This is low risk since this will only be hosted on the LAN
 @app.after_request
 def add_header(response):
     response.headers['X-Frame-Options'] = 'ALLOWALL'
     return response
-
+    
 if __name__ == "__main__":
     app.run()
 
